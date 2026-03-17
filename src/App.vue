70739192012669b6e3ee69f1a01c3bdfab7d8402
Matheus-Mxla <matheuslacerdamaximiano@gmail.com>
2026-03-17 09:30:34 -0300

    Pronto

--- src/App.vue
+++ src/App.vue
@@ -1,55 +1,111 @@
 <script setup>
-  import { ref } from 'vue'
+  import { ref, computed } from 'vue';
   const tarefas = ref([
   {
     id: 1,
-    tarefa: 'Tarefa 1',
+    tarefa: '1 Supino Inclinado',
     status: 'concluida',
   },
   {
     id: 2,
-    tarefa: 'Tarefa 2',
+    tarefa: '2 Crucifixo no Voador',
     status: 'concluida',
   },
   {
     id: 3,
-    tarefa: 'Tarefa 3',
+    tarefa: '3 Supino Reto',
     status: 'pendente',
   },
   {
     id: 4,
-    tarefa: 'Tarefa 4',
+    tarefa: '4 Elevação Lateral',
+    status: 'pendente',
+  },
+  {
+    id: 5,
+    tarefa: '5 Desenvolvimento Halter',
+    status: 'pendente',
+  },
+  {
+    id: 6,
+    tarefa: '6 Triceps Frances',
+    status: 'pendente',
+  },
+  {
+    id: 7,
+    tarefa: '7 Triceps na Polia',
     status: 'pendente',
   },
 ])
-const novaTarefa = ref ("");
+const novaTarefa = ref ('');
+const posiAlterada = ref (null);
 
-function addTarefa(item) {
-  tarefas.value.push({id: tarefas.value[tarefas.value.length -1].id + 1, tarefa: item, status: 'pendente'})
+function addTarefa() {
+  if (novaTarefa.value.trim().length >= 5) {
+    if (posiAlterada.value !== null) {
+      tarefas.value[posiAlterada.value].tarefa = novaTarefa.value;
+      posiAlterada.value = null;
+    }
+    else {
+      tarefas.value.push({
+        id: tarefas.value.length + 1,
+        tarefa: novaTarefa.value,
+        status: 'pendente'
+      });
+    }
+    novaTarefa.value = '';
+  }
 }
 function delTarefa(item) {
   let i = tarefas.value.indexOf(item);
   tarefas.value.splice(i, 1);
 }
-function ordenarTarefa() {
-
+function editTarefa(item) {
+  posiAlterada.value = tarefas.value.indexOf(item);
+  novaTarefa.value = item.tarefa;
 }
+
+function concluirLista(item) {
+  item.status = item.status === 'concluida' ? 'pendente' : 'concluida';
+}
+function ordenarTarefa () {
+  tarefas.value.sort((a, b) => a.tarefa.localeCompare(b.tarefa))
+}
+const filtro = ref ('')
+const tarefaFiltrada = computed(() => {
+  if (filtro.value.trim().length > 0) {
+    return tarefas.value.filter(item => item.tarefa.includes(filtro.value));
+  }
+  else {
+    return tarefas.value
+  }
+})
 </script>
 
 <template>
   <div class="container">
     <h1>Lista de Tarefas</h1>
     <input type="text" v-model="novaTarefa">
-    <button @click="addTarefa(novaTarefa)">Add-Tarefa</button>
-    <button @click="ordenarTarefa()">Ordenar-Tarefa</button>
+    <button @click="addTarefa()">Adicionar</button>
+    <button @click="ordenarTarefa()">Ordenar</button>
     <ul>
-      <li v-for="item in tarefas" :key="item.id">
+      <li v-for="item in tarefaFiltrada" :key="item.id" :class="{ concluida: item.status === 'concluida' }">
         {{ item.tarefa }}
-        <button @click="delTarefa(item)">Del-Tarefa</button>
+        <button @click="delTarefa(item)">Deletar</button>
+        <button @click="editTarefa(item)">Editar</button>
+        <button href="#" @click="concluirLista(item)">Concluida</button>
       </li>
     </ul>
+    <input type="text" v-model="filtro">
   </div>
 </template>
 
 <style scoped>
+.concluida {
+  text-decoration: line-through;
+  color: rgb(69, 197, 75);
+}
+li {
+  color: rgb(207, 67, 67);
+}
 </style>
